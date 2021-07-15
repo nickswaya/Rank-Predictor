7b7913607c17720d6d5a59bacd3f2102d8d6c0ed
Nickswaya <nickswaya@github.com>
2021-07-14 17:47:28 -0700

    added validation for data entry

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
 from wtforms.validators import DataRequired
 
 class SubmitReplay(FlaskForm):
-    replay_id = StringField('Replay ID', validators=[DataRequired()])
+    replay_id = StringField('Replay ID', validators=[DataRequired(), validators.Length(36, message='Match ID not valid: Length Error')])
     submit = SubmitField('Submit Replay ID')
